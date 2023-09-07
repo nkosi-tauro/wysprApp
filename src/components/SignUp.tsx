971fc7cd7cf34e4e3e953e8da81bfa2b0bf5ed14
nkosi-tauro <nkosilati23@yahoo.com>
2023-09-07 15:16:01 +0200

    added Login tenary

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Home from "../pages/Home";
+import Login from "./Login";
+
 
 
 export default function SignUp() {
@@ -8,14 +11,22 @@ export default function SignUp() {
   const confirmPassword = useRef<HTMLInputElement>(null)
 
   const [redirectHome, setRedirectHome] = useState<boolean>(false)
-  const signUp = localStorage.getItem('signUp')
+  // Show Login Instead of SignUp if Account Exists (in LocalStorage)
+  const [showLogin, setShowLogin] = useState(false)
 
-  // useEffect(() => {
-  //   // go to home if signUp object exists in LocalStorage
-  //   if (signUp) {
-  //     setRedirectHome(true)
-  //   }
-  // })
+  const signUp = localStorage.getItem('signUp')
+  const localUsername = localStorage.getItem('username')
+
+  useEffect(() => {
+    // go to home if signUp object exists in LocalStorage
+    if (signUp) {
+      setRedirectHome(true)
+    }
+    //show Login Page when user account exists 
+    if (localUsername) {
+      setShowLogin(true)
+    }
+  })
 
   const handleSignUp = () => {
     // Could add null check here... 
@@ -35,81 +46,83 @@ export default function SignUp() {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-            Sign Up
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="space-y-6" >
-            <div>
-              <label htmlFor="text" className="block text-sm font-medium leading-6 text-white">
-                username
-              </label>
-              <div className="mt-2">
-                <input
-                  ref={username}
-                  id="text"
-                  name="text"
-                  type="text"
-                  autoComplete="text"
-                  required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
+      {redirectHome ? <Home /> :
+        (showLogin ? <Login /> :
+          <div className=" flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+                Sign Up
+              </h2>
             </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  ref={password}
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <div className="space-y-6" >
+                <div>
+                  <label htmlFor="text" className="block text-sm font-medium leading-6 text-white">
+                    username
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      ref={username}
+                      id="text"
+                      name="text"
+                      type="text"
+                      autoComplete="text"
+                      required
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  ref={confirmPassword}
-                  id="confirmPassword"
-                  name="confirmpassword"
-                  type="password"
-                  required
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                      Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      ref={password}
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
-            <div>
-              <button
-                onClick={handleSignUp}
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Sign Up 
-              </button>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                      Confirm Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      ref={confirmPassword}
+                      id="confirmPassword"
+                      name="confirmpassword"
+                      type="password"
+                      required
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    onClick={handleSignUp}
+                    className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        )
+      }
     </>
   )
-
-
 }
