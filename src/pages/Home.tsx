30a316f59ca1b111f973c645582d454a23de4e40
nkosi-tauro <nkosilati23@yahoo.com>
2023-09-07 15:16:17 +0200

    added base home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+
+export default function Home(){
+  return (
+    <>
+      <Navbar/>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </>
+  )
+}
